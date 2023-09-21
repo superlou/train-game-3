e87d27c5caef077114ce7f51fe1cd233a6a5729c
superlou <lousimons@gmail.com>
2023-09-21 19:33:55 -0400

    Initial commit

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+@export var walk_speed = 200
+@export var sprint_speed = 500
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
+
+
+func _physics_process(_delta):
+	var v = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var speed = sprint_speed if Input.is_action_pressed("sprint") else walk_speed
+	velocity = v * speed
+	move_and_slide()
