463d920c0a81ef1343dd4a358e43c3353ba2d7f4
superlou <lousimons@gmail.com>
2023-10-25 19:37:17 -0400

    Moved train into world to make it easier to move objects between different cars and the ground

--- train.gd
+++ train.gd
@@ -1,21 +1,11 @@
 extends Node2D
 
-
 @export var velocity = 500
-var reparenting_watches = []
-var leaving_train_watches = []
-@export var player: Node2D = null
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = %Player
-	for car in %Cars.get_children():
-		car.connect("body_entered", func(b): _on_body_entered(b, car))
-		car.connect("body_exited", func(b): _on_body_exited(b, car))
-
-
-func _process(_delta):
-	%Camera.global_position = player.global_position
+	pass
 
 
 func _physics_process(delta):
@@ -24,69 +14,3 @@ func _physics_process(delta):
 	if $Track.position.x < -32*2:
 		$Track.position.x = 0
 
-	_handle_reparenting()
-	_handle_leaving_train()
-	
-	for child in %Ground.get_children():
-		child.position.x -= velocity * delta
-
-	var dist_from_broadcast := player.global_position.distance_to(%Broadcast.global_position)
-	if dist_from_broadcast > 1500:
-		respawn()
-
-
-func _handle_reparenting():
-#	if len(reparenting_watches) > 0:
-#		print("Reparenting watches: ", reparenting_watches)
-
-	var remaining_watches = []
-	
-	for body in reparenting_watches:
-		var containing_cars = %Cars.get_children().filter(func(car): return car.contains(body))
-
-		if len(containing_cars) == 1:
-			var new_parent = containing_cars[0]
-			if body.get_parent() != new_parent:
-				body.reparent(new_parent)
-		elif len(containing_cars) == 0:
-			pass # Probably leaving the train. Don't track anymore
-		else:
-			remaining_watches.append(body)
-
-	reparenting_watches = remaining_watches
-
-
-func _handle_leaving_train():
-	var remaining_watches = []
-
-	for body in leaving_train_watches:
-		var containing_cars = %Cars.get_children().filter(func(car): return car.contains(body))
-		
-		if len(containing_cars) == 0 and body.get_collision_layer_value(6):
-			body.reparent(%Ground)
-		else:
-			remaining_watches.append(body)
-
-	leaving_train_watches = remaining_watches
-
-
-func _on_body_entered(body, car):
-	# print("enter: ", body, car)
-	if body.get_parent() != car and body not in reparenting_watches:
-		reparenting_watches.append(body)
-
-
-func _on_body_exited(body, _car):
-	# print("exit: ", body, car)
-	leaving_train_watches.append(body)
-
-
-func respawn():
-	var tween = get_tree().create_tween()
-	%Camera.position_smoothing_enabled = false
-	tween.tween_property(%Camera/Curtain, "color", Color(0, 0, 0, 1), 0.5)
-	tween.tween_interval(1)
-	tween.tween_callback(func(): player.global_position = %Respawn.global_position)
-	tween.tween_interval(1)
-	tween.tween_property(%Camera/Curtain, "color", Color(0, 0, 0, 0), 0.5)
-	tween.tween_callback(func(): %Camera.position_smoothing_enabled = true)
--- world.gd
+++ world.gd
@@ -1,5 +1,8 @@
 extends Node2D
 
+@export var player: Node2D = null
+var reparenting_watches = []
+var leaving_train_watches = []
 
 var Station = load("res://station.tscn")
 var station = null
@@ -15,10 +18,17 @@ var travel_state: TravelType = TravelType.TRAVELING
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = %Player
+
+	for car in %Cars.get_children():
+		car.connect("body_entered", func(b): _on_body_entered(b, car))
+		car.connect("body_exited", func(b): _on_body_exited(b, car))
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	%Camera.global_position = player.global_position
+
+
 func _physics_process(delta):
 	if Input.is_action_just_pressed("spawn_station"):
 		station = Station.instantiate()
@@ -35,5 +45,70 @@ func _physics_process(delta):
 			%Train/Cars/Flatbed1/Ramp.deploy()
 			station.unfreeze_bodies()
 
+	_handle_reparenting()
+	_handle_leaving_train()
+
 	if station:
 		station.position.x -= %Train.velocity * delta
+
+	for child in %Ground.get_children():
+		child.position.x -= %Train.velocity * delta
+
+	var dist_from_broadcast := player.global_position.distance_to(%Broadcast.global_position)
+	if dist_from_broadcast > 1500:
+		respawn()
+
+
+func _handle_reparenting():
+	var remaining_watches = []
+	
+	for body in reparenting_watches:
+		var containing_cars = %Cars.get_children().filter(func(car): return car.contains(body))
+
+		if len(containing_cars) == 1:
+			var new_parent = containing_cars[0]
+			if body.get_parent() != new_parent:
+				body.reparent(new_parent)
+		elif len(containing_cars) == 0:
+			pass # Probably leaving the train. Don't track anymore
+		else:
+			remaining_watches.append(body)
+
+	reparenting_watches = remaining_watches
+	
+	
+func _handle_leaving_train():
+	var remaining_watches = []
+
+	for body in leaving_train_watches:
+		var containing_cars = %Cars.get_children().filter(func(car): return car.contains(body))
+		
+		if len(containing_cars) == 0 and body.get_collision_layer_value(6):
+			body.reparent(%Ground)
+		else:
+			remaining_watches.append(body)
+
+	leaving_train_watches = remaining_watches
+
+
+func _on_body_entered(body, car):
+	print("enter: ", body, car)
+	if body.get_parent() != car and body not in reparenting_watches:
+		reparenting_watches.append(body)
+
+
+func _on_body_exited(body, _car):
+	# print("exit: ", body, car)
+	leaving_train_watches.append(body)
+
+
+func respawn():
+	var tween = get_tree().create_tween()
+	%Camera.position_smoothing_enabled = false
+	tween.tween_property(%Camera/Curtain, "color", Color(0, 0, 0, 1), 0.5)
+	tween.tween_interval(1)
+	tween.tween_callback(func(): player.global_position = %Respawn.global_position)
+	tween.tween_interval(1)
+	tween.tween_property(%Camera/Curtain, "color", Color(0, 0, 0, 0), 0.5)
+	tween.tween_callback(func(): %Camera.position_smoothing_enabled = true)
+		
