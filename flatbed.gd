756b41728360e628f4ea225b550000c333403536
superlou <lousimons@gmail.com>
2023-10-26 20:37:14 -0400

    Added decoupling cars

--- flatbed.gd
+++ flatbed.gd
@@ -1,6 +1,9 @@
 extends Platform
 
 
+@export var relative_velocity := 0.0
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	super()
--- train.gd
+++ train.gd
@@ -1,11 +1,13 @@
 extends Node2D
 
-@export var velocity = 500
+@export var velocity := 500.0
+const ROLLING_FRICTION := 10.0
+@export var decoupled_cars = []
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	for car in %Cars.get_children():
+		car.relative_velocity = 0
 
 
 func _physics_process(delta):
@@ -14,3 +16,9 @@ func _physics_process(delta):
 	if $Track.position.x < -32*2:
 		$Track.position.x = 0
 
+	for car in decoupled_cars:
+		car.relative_velocity -= ROLLING_FRICTION * delta
+		car.relative_velocity = max(car.relative_velocity, -velocity)
+
+	for car in %Cars.get_children():
+		car.position.x += car.relative_velocity * delta
